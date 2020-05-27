eb5618af7d390f2f500c9e6babb4fdf071b80b0e
1912655018 <1912655018@qq.com>
2020-05-28 00:15:00 +0800

    修改

--- spring-mytest/src/main/java/com/yedi/spring/Mytest.java
+++ spring-mytest/src/main/java/com/yedi/spring/Mytest.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Mytest {
 	public static void main(String[] args) {
-//		AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(JavaConfig.class);
+		AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(JavaConfig.class);
 		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("application.xml");
 		User bean = applicationContext.getBean(User.class);
 		System.out.println(bean);
